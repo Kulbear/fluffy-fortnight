67d419f9c33fe13425fa32300a48a7874732ffb6
Kulbear <jiyang.py@gmail.com>
2019-03-13 23:55:54 -0600

    0125, 0680

---
+++ 0125_ValidPalindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    # Solution 1 - Two Pointers
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        l, r = 0, len(s) - 1
+        while l < r:
+            # skipping invalid chars
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
